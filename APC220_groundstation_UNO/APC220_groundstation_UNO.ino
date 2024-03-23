a11589663f3bdaedc4c1e324476116956d052a31
owiedepowie <101893046+owiedepowie@users.noreply.github.com>
2024-03-23 12:51:46 +0100

    Added libraries folder and moved other code to folders

---
+++ APC220_groundstation_UNO/APC220_groundstation_UNO.ino
@@ -0,0 +1,30 @@
+/*
+  Modified on March 09, 2021
+  Modified by MohammedDamirchi from https://github.com/PaulStoffregen/SoftwareSerial
+  Home
+*/
+
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2, 3); // RX, TX
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+}
+
+void loop() { // run over and over
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
+  if (Serial.available()) {
+    mySerial.write(Serial.read());
+  }
+}
